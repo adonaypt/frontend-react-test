5fb9e67589280f2025934c91b211706399e37938
adonaypt <adonaypt1999@gmail.com>
2026-03-15 14:28:29 +0000

    refactor: added constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://itx-frontend-test.onrender.com'
+export const STORAGE_KEY = 'cart_count'
+export const CACHE_TTL = 60 * 60 * 1000 // 1h
+export const CACHE_KEY_PRODUCTS = 'products'
+export const CACHE_KEY_PRODUCT_DETAIL = 'product_detail'
